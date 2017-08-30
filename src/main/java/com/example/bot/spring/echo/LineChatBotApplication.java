b5813a823f93d72a5720f8dacf904faa05a75381
anzu223 <chi.sugiura.223@gmail.com>
2017-08-31 08:19:04 +0900

    first commit

---
+++ src/main/java/com/example/bot/spring/echo/LineChatBotApplication.java
@@ -0,0 +1,12 @@
+package com.example.bot.spring.echo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineChatBotApplication.class, args);
+	}
+}
